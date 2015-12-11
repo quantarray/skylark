882d24d12ccfa9d86964493adf9a174c7c43db8b
araik <araik@quantarray.com>
2015-12-10 19:56:14 -0500

    Tidy up TimeSeriesPoint.

--- skylark-timeseries/src/main/scala/com/quantarray/skylark/timeseries/TimeSeriesPoint.scala
+++ skylark-timeseries/src/main/scala/com/quantarray/skylark/timeseries/TimeSeriesPoint.scala
@@ -41,15 +41,15 @@ object TimeSeriesPoint
 {
   def apply[V](observedTime: DateTime, observedValue: V, versionTime: DateTime = DateTime.now()): TimeSeriesPoint[V] =
   {
-    val otovvt = (observedTime, observedValue, versionTime)
+    val params = (observedTime, observedValue, versionTime)
 
     new TimeSeriesPoint[V]
     {
-      val observedTime: DateTime = otovvt._1
+      val observedTime: DateTime = params._1
 
-      val observedValue: V = otovvt._2
+      val observedValue: V = params._2
 
-      val versionTime: DateTime = otovvt._3
+      val versionTime: DateTime = params._3
     }
   }
 }
